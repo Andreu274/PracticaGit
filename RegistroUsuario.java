b954d76abb5dd5309311b3a3d5c8299927c02f2c
Andreu274 <andreupons@paucasesnovescifp.cat>
2023-03-28 11:32:30 +0200

    Afegesc els meus dos mètodes i els seus respectius tests

--- RegistroUsuario.java
+++ RegistroUsuario.java
@@ -1,32 +1,27 @@
 import java.util.Scanner;
 
 public class RegistroUsuario {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        ValidarCampos validador = new ValidarCampos();
 
-        // Ingreso del nombre
-        System.out.println("Ingrese su nombre: ");
+        System.out.print("Ingrese su nombre: ");
         String nombre = sc.nextLine();
-
-        // Ingreso del email
-        System.out.println("Ingrese su email: ");
+        System.out.print("Ingrese su email: ");
         String email = sc.nextLine();
-
-        // Ingreso de la contraseña
-        System.out.println("Ingrese su contraseña: ");
+        System.out.print("Ingrese su password: ");
         String password = sc.nextLine();
-
-        // Ingreso del código
-        System.out.println("Ingrese su código: ");
+        System.out.print("Ingrese su código: ");
         String codigo = sc.nextLine();
 
-        // Imprimir los datos ingresados
-        System.out.println("Los datos ingresados son: ");
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Email: " + email);
-        System.out.println("Contraseña: " + password);
-        System.out.println("Código: " + codigo);
-
-        sc.close();
+        boolean camposCorrectos = validador.validarCampos(nombre, email, password, codigo);
+        if (camposCorrectos) {
+            System.out.println("Campos correctos");
+        }
+        else {
+            System.out.println("Algun campo es incorrecto");
+        }
     }
 }
--- ValidarCampos.java
+++ ValidarCampos.java
@@ -1,66 +1,56 @@
 public class ValidarCampos {
-    public boolean validarCampos(String nombre, String email, String password, String codigo) {
-        boolean comprubaNombre =  compruebaNombre(nombre);
-        boolean compruebaEmail = compruebaEmail(email);
+    public boolean validarCampos(String nombreUsuario, String email, String password, String codigo) {
+        boolean comprubaNombre =  compruebaNombre(nombreUsuario);
         boolean compruebaPassword = compruebaPassword(password);
-        boolean compruebaCodigo = compruebaCodigo(codigo);
 
-        return comprubaNombre && compruebaEmail && compruebaPassword && compruebaCodigo;
+        return comprubaNombre && compruebaPassword ;
     }
 
-    private boolean compruebaNombre(String nombre) {
-        if(nombre.isEmpty()) {
-            System.out.println("El nombre no puede estar vacío.");
+    public static boolean compruebaNombre(String nombreUsuario) {
+        if(nombreUsuario.isEmpty()) {
+            System.out.println("El nombre de usuario no puede estar vacío.");
             return false;
+        }
+
+        if(nombreUsuario.length() > 16) {
+            System.out.println("El nombre de usuario no puede tener más de 16 caracteres.");
+            return false;
+        }
+
+        if(!nombreUsuario.matches("[A-Z][a-z]+[_-]\\d{3}")) {
+            System.out.println("El nombre de usuario debe cumplir el formato: una mayúscula seguida de minúsculas, un guión medio o bajo y tres dígitos.");
+            return false;
+        }
+
+        String[] nombresExistentes = {"Carles_345", "Pablo-234", "Luisa_987", "Marcos_123", "Maria_456", "Ana-678", "Juan_098", "Pedro-564", "Lucia-237", "Sara_765"};
+
+        for(int i = 0; i < nombresExistentes.length; i++) {
+            if(nombresExistentes[i].equalsIgnoreCase(nombreUsuario)) {
+                System.out.println("El nombre de usuario ya existe en la base de datos.");
+                return false;
+            }
         }
 
         return true;
     }
 
-    private boolean compruebaEmail(String email) {
-        if(email.isEmpty()) {
-            System.out.println("El email no puede estar vacío.");
-            return false;
-        }
-
-        if(!email.matches("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
-            System.out.println("El email ingresado no es válido.");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean compruebaPassword(String password) {
+    public static boolean compruebaPassword(String password) {
         if(password.isEmpty()) {
-            System.out.println("El password no puede estar vacío.");
+            System.out.println("La contraseña no puede estar vacía.");
             return false;
         }
 
-        if(password.length() < 8) {
-            System.out.println("El password debe tener al menos 8 caracteres.");
+        if(password.length() <= 8) {
+            System.out.println("La contraseña debe tener 8 caracteres.");
             return false;
         }
 
-        return true;
-    }
-
-    private boolean compruebaCodigo(String codigo) {
-        if(codigo.isEmpty()) {
-            System.out.println("El código no puede estar vacío.");
-            return false;
-        }
-
-        if(codigo.length() != 6) {
-            System.out.println("El código debe tener 6 caracteres.");
-            return false;
-        }
-
-        if(!codigo.matches("[0-9]+")) {
-            System.out.println("El código debe ser numérico.");
+        if(!password.matches("[A-Z][a-zA-Z0-9]*[@\\-_#][0-9]{2}")) {
+            System.out.println("La contraseña debe cumplir el formato: una mayúscula seguida de letras, números y un carácter especial (@,-,# o _) seguido de dos dígitos.");
             return false;
         }
 
         return true;
     }
+
 }
