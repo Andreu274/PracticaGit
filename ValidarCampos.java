880c96c68fa1ae2cc637f5697f20960534921890
Marc <marcsans26@gmail.com>
2023-03-28 11:26:04 +0200

    cream els metodes per compobar el correu i el codi i cream les proves de junit

--- ValidarCampos.java
+++ ValidarCampos.java
@@ -1,29 +1,18 @@
 public class ValidarCampos {
     public boolean validarCampos(String nombre, String email, String password, String codigo) {
-        boolean comprubaNombre =  compruebaNombre(nombre);
         boolean compruebaEmail = compruebaEmail(email);
-        boolean compruebaPassword = compruebaPassword(password);
         boolean compruebaCodigo = compruebaCodigo(codigo);
 
-        return comprubaNombre && compruebaEmail && compruebaPassword && compruebaCodigo;
+        return compruebaEmail && compruebaCodigo;
     }
 
-    private boolean compruebaNombre(String nombre) {
-        if(nombre.isEmpty()) {
-            System.out.println("El nombre no puede estar vacío.");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean compruebaEmail(String email) {
+    public boolean compruebaEmail(String email) {
         if(email.isEmpty()) {
             System.out.println("El email no puede estar vacío.");
             return false;
         }
 
-        if(!email.matches("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
+        if(!email.matches("[\\w]+@(paucasesnovescifp|yahoo|gmail|hotmail)\\.(com|es|cat)")) {
             System.out.println("El email ingresado no es válido.");
             return false;
         }
@@ -31,33 +20,14 @@ public class ValidarCampos {
         return true;
     }
 
-    private boolean compruebaPassword(String password) {
-        if(password.isEmpty()) {
-            System.out.println("El password no puede estar vacío.");
-            return false;
-        }
-
-        if(password.length() < 8) {
-            System.out.println("El password debe tener al menos 8 caracteres.");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean compruebaCodigo(String codigo) {
+    public boolean compruebaCodigo(String codigo) {
         if(codigo.isEmpty()) {
             System.out.println("El código no puede estar vacío.");
             return false;
         }
 
-        if(codigo.length() != 6) {
-            System.out.println("El código debe tener 6 caracteres.");
-            return false;
-        }
-
-        if(!codigo.matches("[0-9]+")) {
-            System.out.println("El código debe ser numérico.");
+        if(!codigo.matches("[a-zA-Z0-9!@#$%^&*()_+={}|;':,./<>?]{8}")) {
+            System.out.println("El codigo no es válido.");
             return false;
         }
 
